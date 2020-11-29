7d5d0ed179fd730c9544f1102be0f8bf544ef379
Taiyo Yamaguchi <onikun94@gmail.com>
2020-11-29 18:51:22 +0900

    add answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import defaultDataset from "./dataset"
 import './assets/styles/style.css'
+import {AnswersList} from "./components/index";
 
 export default class App extends React.Component {
   constructor(props){
@@ -13,12 +14,24 @@ export default class App extends React.Component {
       open: false
     }
   }
+
+  initAnswer = () =>{
+    const initADataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initADataset.answers;
+    this.setState({
+      answers: initAnswers
+    })
+  }
   
+  componentDidMount(){
+    this.initAnswer();
+  }
+
   render(){
     return (
       <section className = "c-section">
         <div className = "c-box">
-          {this.state.currentId}
+          <AnswersList answers={this.state.answers}/>
         </div>
       </section>
 
---
+++ src/components/AnswersList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//import '../assets/styles/style.css'
+import {Answer} from './index'
+
+const AnswersList = (props) =>{
+    return (
+        <div className="c-grid__answer">
+            {props.answers.map((value, index)=>{
+                return <Answer content = {value.content} key = {index.toString()}/>
+            })}
+        </div>
+    ) 
+}
+
+export default AnswersList
