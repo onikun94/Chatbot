140678775f1c03ecc2d841173a1fb133629e9a88
Taiyo Yamaguchi <onikun94@gmail.com>
2020-12-08 22:26:16 +0900

    revise code

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,46 @@ export default class App extends React.Component {
     }
   }
 
-  initAnswer = () =>{
-    const initADataset = this.state.dataset[this.state.currentId];
-    const initAnswers = initADataset.answers;
+
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    })
+
     this.setState({
-      answers: initAnswers
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
     })
   }
 
-  initChats = () => {
-    const initDataset = this.state.dataset[this.state.currentId];
-    const chat = {
-      text: initDataset.question,
-      type: 'question'
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId);
+        break;
+
+      default:
+        const chats = this.state.chats;
+        chats.push({
+          text:selectedAnswer,
+          type: 'ansewer'
+        })
+        this.setState({
+          chas: chats
+        })
+        this.displayNextQuestion(nextQuestionId);
+        break;
     }
-
-    const chats = this.state.chats;
-    chats.push(chat)
-
-    this.setState({
-        chats:chats
-      })
   }
+
+
   
   componentDidMount(){
-    this.initChats();
-    this.initAnswer();
+    const initAnswer =  "";
+    this.selectAnswer(initAnswer, this.state.currentId)
   }
 
   render(){
@@ -48,7 +62,7 @@ export default class App extends React.Component {
       <section className = "c-section">
         <div className = "c-box">
           <Chats chats = {this.state.chats} />
-          <AnswersList answers={this.state.answers}/>
+          <AnswersList answers={this.state.answers} select = {this.selectAnswer}/>
         </div>
       </section>
 
